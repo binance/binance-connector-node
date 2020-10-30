bfb3b8ee987c83d4dc0fc4e0e1c585b4c5967079
Liang Shi <liang.shi@protonmail.com>
2020-10-30 14:54:30 +1100

    add mining endpoints

---
+++ __tests__/spot/mining/miningRevenueList.test.js
@@ -0,0 +1,44 @@
+/* global describe, it, expect, */
+const MissingParameterError = require('../../../src/error/missingParameterError')
+
+const {
+  nockMock,
+  responseMockData,
+  SpotClient
+} = require('../../testUtils/testSetup')
+
+const {
+  queryString
+} = require('../../testUtils/mockData')
+
+const algo = 'sha256'
+const userName = 'minerName'
+
+describe('#miningRevenueList', () => {
+  describe('throw MissingParameterError', () => {
+    it('missing algo', async () => {
+      expect(() => {
+        SpotClient.miningRevenueList('', userName)
+      }).toThrow(MissingParameterError)
+    })
+
+    it('missing userName', async () => {
+      expect(() => {
+        SpotClient.miningRevenueList(algo, '')
+      }).toThrow(MissingParameterError)
+    })
+  })
+
+  it('should return miner list', async () => {
+    const parameters = {
+      algo,
+      userName
+    }
+    nockMock(`/sapi/v1/mining/payment/list${queryString(parameters)}`)(responseMockData)
+
+    return SpotClient.miningRevenueList(algo, userName).then(response => {
+      expect(response).toBeDefined()
+      expect(response.data).toEqual(responseMockData)
+    })
+  })
+})
---
+++ __tests__/spot/mining/miningStatisticList.test.js
@@ -0,0 +1,44 @@
+/* global describe, it, expect, */
+const MissingParameterError = require('../../../src/error/missingParameterError')
+
+const {
+  nockMock,
+  responseMockData,
+  SpotClient
+} = require('../../testUtils/testSetup')
+
+const {
+  queryString
+} = require('../../testUtils/mockData')
+
+const algo = 'sha256'
+const userName = 'minerName'
+
+describe('#miningStatisticList', () => {
+  describe('throw MissingParameterError', () => {
+    it('missing algo', async () => {
+      expect(() => {
+        SpotClient.miningStatisticList('', userName)
+      }).toThrow(MissingParameterError)
+    })
+
+    it('missing userName', async () => {
+      expect(() => {
+        SpotClient.miningStatisticList(algo, '')
+      }).toThrow(MissingParameterError)
+    })
+  })
+
+  it('should return Statistic list', async () => {
+    const parameters = {
+      algo,
+      userName
+    }
+    nockMock(`/sapi/v1/mining/statistics/user/status${queryString(parameters)}`)(responseMockData)
+
+    return SpotClient.miningStatisticList(algo, userName).then(response => {
+      expect(response).toBeDefined()
+      expect(response.data).toEqual(responseMockData)
+    })
+  })
+})
