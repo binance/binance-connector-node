f9af70ff6c1c712444fd7db15fc4bdf24b9d3517
ishuen <12228644+ishuen@users.noreply.github.com>
2021-05-24 08:34:11 +0800

    add index to modules

--- src/spot.js
+++ src/spot.js
@@ -1,18 +1,12 @@
 const APIBase = require('./APIBase')
-const Blvt = require('./modules/blvt')
-const Bswap = require('./modules/bswap')
-const Corporate = require('./modules/corporate')
-const Market = require('./modules/market')
-const Trade = require('./modules/trade')
-const Wallet = require('./modules/wallet')
-const Margin = require('./modules/margin')
-const Mining = require('./modules/mining')
-const Savings = require('./modules/savings')
-const Stream = require('./modules/stream')
-const Websocket = require('./modules/websocket')
+const {
+  Blvt, Bswap, Corporate, Market, Trade,
+  Wallet, Margin, Mining, Savings, Stream, Websocket
+} = require('./modules')
 const flowRight = require('lodash.flowright')
 
-class Spot extends flowRight(Blvt, Bswap, Corporate, Websocket, Stream, Savings, Margin, Mining, Wallet, Market, Trade) (APIBase) {
+class Spot extends flowRight(Blvt, Bswap, Corporate, Websocket, Stream,
+  Savings, Margin, Mining, Wallet, Market, Trade)(APIBase) {
   constructor (apiKey = '', apiSecret = '', options = {}) {
     options.baseURL = options.baseURL || 'https://api.binance.com'
     super({
