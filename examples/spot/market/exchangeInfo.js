bddb751513f71d173f5cb8b3b8f642d0b80edaa9
ishuen <12228644+ishuen@users.noreply.github.com>
2021-05-25 10:06:08 +0800

    add inline documents

--- examples/spot/market/exchangeInfo.js
+++ examples/spot/market/exchangeInfo.js
@@ -3,3 +3,5 @@ const Spot = require('../../../src/spot')
 const client = new Spot('', '')
 
 client.exchangeInfo().then(response => console.log(response.data))
+client.exchangeInfo({symbol: 'btcusdt'}).then(response => console.log(response.data))
+client.exchangeInfo({symbols: ['btcusdt', 'BNBUSDT']}).then(response => console.log(response.data))
