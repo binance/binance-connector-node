0dfd16bcb68aa1dda25deff3baaee76a6a639c1c
ishuen <12228644+ishuen@users.noreply.github.com>
2021-06-21 14:53:05 +0800

    add script to generate changelog

--- refineDoc.js
+++ refineDoc.js
@@ -1,11 +1,26 @@
 const fs = require('fs')
-fs.readFile('./docs/binance-connector-node/0.5.1/index.html', 'utf8', function (err, data) {
+const marked = require('marked')
+const basePath = './docs/binance-connector-node/0.5.1/'
+const indexFile = 'index.html'
+const changeLogFile = 'changelog.html'
+fs.readFile(basePath + indexFile, 'utf8', function (err, data) {
   if (err) {
     return console.log(err)
   }
 
   const result = data.replace('Home', 'binance-connector-node')
-  fs.writeFile('./docs/binance-connector-node/0.5.1/index.html', result, 'utf8', function (err) {
+  fs.writeFile(basePath + indexFile, result, 'utf8', function (err) {
+    if (err) return console.log(err)
+  })
+
+  const changeloMd = fs.readFileSync('CHANGELOG.md', 'utf-8')
+  const changelogContent = marked(changeloMd)
+  const changelog = data.replace(
+    /<section class="readme">\s*<article>[\s\S]+<\/article>\s*<\/section>/,
+    '<section class="readme"> <article>' + changelogContent + '</article> </section>'
+  )
+
+  fs.writeFile(basePath + changeLogFile, changelog, 'utf8', function (err) {
     if (err) return console.log(err)
   })
 })
