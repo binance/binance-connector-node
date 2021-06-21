2e128fda67b6f1a5de0cb63c360f0fbe9fa55ebc
aisling11 <19312514+aisling11@users.noreply.github.com>
2021-06-21 10:49:45 +0100

    add new stream endpoints

--- src/modules/stream.js
+++ src/modules/stream.js
@@ -1,3 +1,5 @@
+const { validateRequiredParameters } = require('../helpers/validation')
+
 /**
  * API stream endpoints
  * @module Stream
@@ -53,9 +55,9 @@ const Stream = superclass => class extends superclass {
   }
 
   /**
-   * Create a Margin ListenKey (USER_STREAM)
+   * Create a Margin ListenKey (USER_STREAM)<br>
    *
-   * POST /sapi/v1/userDataStream
+   * POST /sapi/v1/userDataStream<br>
    *
    * {@link https://binance-docs.github.io/apidocs/spot/en/#listen-key-margin}
    *
@@ -98,6 +100,62 @@ const Stream = superclass => class extends superclass {
       'DELETE',
       '/sapi/v1/userDataStream',
       { listenKey }
+    )
+  }
+
+  /**
+   * Create an Isolated Margin ListenKey (USER_STREAM)<br>
+   *
+   * POST /sapi/v1/userDataStream/isolated<br>
+   *
+   * {@link https://binance-docs.github.io/apidocs/spot/en/#listen-key-isolated-margin}
+   *
+   * @param {string} symbol
+   */
+  createIsolatedMarginListenKey (symbol) {
+    validateRequiredParameters({ symbol })
+    return this.publicRequest(
+      'POST',
+      '/sapi/v1/userDataStream/isolated',
+      { symbol }
+    )
+  }
+
+  /**
+   * Ping/Keep-alive an Isolated Margin ListenKey (USER_STREAM)<br>
+   *
+   * PUT /sapi/v1/userDataStream/isolated<br>
+   *
+   * {@link https://binance-docs.github.io/apidocs/spot/en/#listen-key-isolated-margin}
+   *
+   * @param {string} symbol
+   * @param {string} listenKey
+   */
+  renewIsolatedMarginListenKey (symbol, listenKey) {
+    validateRequiredParameters({ symbol, listenKey })
+    return this.publicRequest(
+      'PUT',
+      '/sapi/v1/userDataStream/isolated',
+      { symbol, listenKey }
+    )
+  }
+
+  /**
+   * Close an Isolated Margin ListenKey (USER_STREAM)<br>
+   *
+   * DELETE /sapi/v1/userDataStream/isolated<br>
+   *
+   * {@link https://binance-docs.github.io/apidocs/spot/en/#listen-key-isolated-margin}
+   *
+   * @param {string} symbol
+   * @param {string} listenKey
+   */
+  closeIsolatedMarginListenKey (symbol, listenKey) {
+    validateRequiredParameters({ symbol, listenKey })
+    return this.publicRequest(
+      'DELETE',
+      '/sapi/v1/userDataStream/isolated',
+      { symbol, listenKey }
     )
   }
 }
