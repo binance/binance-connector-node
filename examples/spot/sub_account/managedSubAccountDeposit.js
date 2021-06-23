ced0e036ca23298eaed37fde780edfa4512bbfc4
aisling11 <19312514+aisling11@users.noreply.github.com>
2021-06-23 11:30:54 +0100

    add new sub-account endpoints

---
+++ examples/spot/sub_account/managedSubAccountDeposit.js
@@ -0,0 +1,13 @@
+const Spot = require('../../../src/spot')
+
+const apiKey = ''
+const apiSecret = ''
+
+const client = new Spot(apiKey, apiSecret)
+
+client.managedSubAccountDeposit(
+  'alice@test.com',
+  'BNB',
+  10
+).then(response => client.logger.log(response.data))
+  .catch(error => client.logger.error(error))
